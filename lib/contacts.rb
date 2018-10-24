43b46de539a04c1ebe1ff70a0da4ebba9b58982d
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-24 20:51:49 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,9 +14,8 @@ require 'pry'
   #   }
   # }
   
-contacts.each do |person, contact_details_hash|
-  contact_details_hash.each do |attribute, data|
-    binding.pry
-  end
+def remove_strawberry(contacts)
+  contacts.each do |person, contact_details_hash|
+  binding.pry
 end
 end
